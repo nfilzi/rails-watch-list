b2adc4e2af2df89a2e9b5acb6dc4a63f36937d11
Nicolas Filzi <nfilzi.webservices@gmail.com>
2023-11-10 10:39:38 +0900

    Add dotenv-rails

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -26,6 +26,6 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name, :image_url)
+    params.require(:list).permit(:name, :image)
   end
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -5,4 +5,7 @@ class List < ApplicationRecord
   # A list must have a unique name.
   validates :name, presence: true, uniqueness: true
   validates :image_url, presence: true
+
+  has_one_attached :image
+  # has_one_attached :description_image
 end
